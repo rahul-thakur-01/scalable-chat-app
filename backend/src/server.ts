dda1f3fbc47bccf504fff4ddca9558a4799d71ea
rahul_thakur <rahulthakur.developer@gmail.com>
2024-06-22 18:42:17 +0530

    feat: Add Socket.IO for real-time communication

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,21 @@
 import express from 'express';
 import { createServer } from 'http';
+import SocketService from './services/socket';
 
 async function startServer() {
 
     const app = express();
     const port = process.env.PORT || 3000;
     const httpServer = createServer(app);
-
+    
+    const socketService = new SocketService();
+    socketService.getIO().attach(httpServer);
     
     httpServer.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`)
     });
 
+    await socketService.initListeners();
 
 }
 startServer();
---
+++ backend/src/services/socket.ts
@@ -0,0 +1,32 @@
+import {Server, Socket} from 'socket.io';
+
+class SocketService{
+    private _io: Server;
+
+    constructor(){
+        this._io = new Server( {
+            cors: {
+                origin: '*',
+                methods: '*',
+                allowedHeaders: '*'
+            }
+        });
+    }
+
+    public getIO(){
+        return this._io;
+    }
+
+
+    public async initListeners(){
+        const io = this._io;
+        io.on('connection', (socket:Socket) => {
+            console.log('Client connected', socket.id);
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
+        });
+    }
+}
+
+export default SocketService;
